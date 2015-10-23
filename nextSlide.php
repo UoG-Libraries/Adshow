e0f897e5d06a7d3ee3cfa528efb9d01b4a47b052
UoG-Libraries-admin <pgriffiths@glos.ac.uk>
2015-10-23 09:54:02 +0100

    Fixed playlists switching
    The switchng between local and global now works when global playlist is
    empty

--- nextSlide.php
+++ nextSlide.php
@@ -5,28 +5,68 @@
 	$playlist = $_POST['playlist'];
 	$slide = $_POST['slide'];
 
-	// set next slide
-	if ($_POST['slide'] < 8) {
-		$slide++;
+	// local playlist currently playing
+	if ($playlist == 0) {
+	
+		switch ($screen) {
+			case '1':
+				include 'parkData.php';
+				break;
+			case '2':
+				include 'fchData.php';
+				break;
+			case '3':
+				include 'oxstallsData.php';
+				break;
+			case '4':
+				include 'suData.php';
+				break;
+		}
+
+		if ($slide == count($slideArray)) {
+
+			// last slide played, change to global playlist
+			include 'globalData.php';
+
+			if (count($globalSlideArray) == 0) {
+				// no slides in global playlist, start again
+				echo '{"action":"reload"}';
+			} else {
+				$playlist = 1;
+				$slide = 1;				
+				echo json_encode($globalSlideArray[$slide]);
+			}
+			
+		} else {
+
+			//  play next slide
+			$slide++;
+			$slideArray[$slide]['slide'] = $slide;
+			echo json_encode($slideArray[$slide]);
+
+		}
+
+	// global playlist currently playing	
 	} else {
-		$slide = 1;
-	}
 
-	switch ($screen) {
-		case '1':
-			include 'parkData.php';
-			break;
-		case '2':
-			include 'fchData.php';
-			break;
-		case '3':
-			include 'oxstallsData.php';
-			break;
-		case '4':
-			include 'suData.php';
-			break;
-	}
+		include 'globalData.php';
 
-	echo json_encode($slideArray[$slide]);
+		if ($slide == count($globalSlideArray)) {
+
+			// last slide played, start again
+			echo '{"action":"reload"}';
+
+		} else {
+
+			// play next slide
+			$slide++;
+			$globalSlideArray[$slide]['slide'] = $slide;
+
+			echo json_encode($globalSlideArray[$slide]);
+
+		}
+
+	}
+	
 
 ?>
