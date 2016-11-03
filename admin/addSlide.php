b01d13d9f711d744caa164a05cf7025131626541
Raphael Jenni <info@rjenni.ch>
2016-11-03 12:14:26 +0000

    Fixed preview and form in the slide editor

--- admin/addSlide.php
+++ admin/addSlide.php
@@ -34,7 +34,7 @@ include "header.php";
 ?>
 <script src="scripts/slideController.js"></script>
 <div class="row">
-    <div class="col-md-5">
+    <div class="col-md-5 preview-margin-top">
         <div
             class="embed-responsive <?php echo $playlist[0]['screenOrientation'] == '0' ? 'embed-responsive-16by9' : 'embed-responsive-9by16' ?>">
             <iframe class="embed-responsive-item" src=""
@@ -42,17 +42,13 @@ include "header.php";
         </div>
 
     </div>
-    <div class="col-md-offset-1 col-md-6">
+    <div class="col-md-offset-1 col-md-5">
         <form class="template-editor" action="addSlide.php" method="post"
               enctype="application/x-www-form-urlencoded">
             <input type="hidden" name="formSent" value="yes"/>
             <input type="hidden" name="playlistID" value="<?php echo $_GET["playlistID"]; ?>"/>
             <input type="hidden" name="imageURL" id="imageURL"/>
             <input type="hidden" name="templateName" id="templateName" value="<?php echo $templateDirs[0]; ?>"/>
-            <div class="row">
-                <a href="editPlaylist.php?id=<?php echo $_GET["playlistID"] ?>" class="btn btn-primary pull-right">Cancel</a>
-                <input type="submit" class="btn btn-primary pull-right" value="Save"/>
-            </div>
             <div class="row">
                 <div class="form-group">
                     <label for="inputTitle">Title</label>
@@ -101,20 +97,30 @@ include "header.php";
                     </label>
                 </div>
             </div>
+            <div class="row">
+                <a href="editPlaylist.php?id=<?php echo $slide["playlistID"] ?>" class="btn btn-primary">Cancel</a>
+                <input type="submit" class="btn btn-primary" value="Save"/>
+            </div>
         </form>
-        <div class="row imageUploadSection">
-            <form class="uploadform" method="post" enctype="multipart/form-data"
-                  action='upload.php'>
-                <div class="form-group">
-                    <label for="imagefile">Upload your image: </label>
-                    <div class="input-group">
-                        <input type="file" name="imagefile" id="imagefile"/>
+        <div class="h-space2"></div>
+        <div class="row panel panel-default">
+            <div class="panel-heading">
+                Image upload
+            </div>
+            <div class="imageUploadSection panel-body">
+                <form class="uploadform " method="post" enctype="multipart/form-data"
+                      action='upload.php'>
+                    <div class="form-group">
+                        <label for="imagefile">Upload your image: </label>
+                        <div class="input-group">
+                            <input type="file" name="imagefile" id="imagefile"/>
+                        </div>
                     </div>
-                </div>
-                <input type="submit" value="Upload" class="btn btn-default" name="submitbtn" id="submitbtn">
-            </form>
-            <!-- The uploaded image will display here -->
-            <div id='viewimage'></div>
+                    <input type="submit" value="Upload" class="btn btn-default" name="submitbtn" id="submitbtn">
+                </form>
+                <!-- The uploaded image will display here -->
+                <div id='viewimage'></div>
+            </div>
         </div>
     </div>
 </div>
--- admin/editSlide.php
+++ admin/editSlide.php
@@ -34,14 +34,15 @@ include "header.php";
 ?>
 <script src="scripts/slideController.js"></script>
 <div class="row">
-    <div class="col-md-5">
-        <div class="embed-responsive <?php echo $playlist[0]['screenOrientation'] == '0' ? 'embed-responsive-16by9' : 'embed-responsive-9by16' ?>">
+    <div class="col-md-5 preview-margin-top">
+        <div
+            class="embed-responsive <?php echo $playlist[0]['screenOrientation'] == '0' ? 'embed-responsive-16by9' : 'embed-responsive-9by16' ?>">
             <iframe class="embed-responsive-item" src=""
                     id="template-container" name="template-container"></iframe>
         </div>
 
     </div>
-    <div class="col-md-offset-1 col-md-6">
+    <div class="col-md-offset-1 col-md-5">
         <form class="template-editor" action="editSlide.php" method="post"
               enctype="application/x-www-form-urlencoded">
             <input type="hidden" name="formSent" value="yes"/>
@@ -49,10 +50,6 @@ include "header.php";
             <input type="hidden" name="playlistID" value="<?php echo $slide["playlistID"] ?>"/>
             <input type="hidden" name="templateName" id="templateName" value="<?php echo $slide["templateName"] ?>"/>
             <input type="hidden" name="imageURL" id="imageURL" value="<?php echo $slide["imageURL"] ?>"/>
-            <div class="row">
-                <a href="editPlaylist.php?id=<?php echo $slide["playlistID"] ?>" class="btn btn-primary pull-right">Cancel</a>
-                <input type="submit" class="btn btn-primary pull-right" value="Save"/>
-            </div>
             <div class="row">
                 <div class="form-group">
                     <label for="inputTitle">Title</label>
@@ -75,7 +72,7 @@ include "header.php";
                                     <input type="checkbox"
                                            name="enableMarkdown"
                                            id="enableMarkdown"
-                                           <?php echo $slide['markdownEnabled'] ? 'checked' : ''?>
+                                        <?php echo $slide['markdownEnabled'] ? 'checked' : '' ?>
                                            onclick="vm.updatePreview()"> Enable Markdown
                                 </label>
                             </div>
@@ -108,20 +105,30 @@ include "header.php";
                     </label>
                 </div>
             </div>
+            <div class="row">
+                <a href="editPlaylist.php?id=<?php echo $slide["playlistID"] ?>" class="btn btn-primary">Cancel</a>
+                <input type="submit" class="btn btn-primary" value="Save"/>
+            </div>
         </form>
-        <div class="row imageUploadSection">
-            <form class="uploadform " method="post" enctype="multipart/form-data"
-                  action='upload.php'>
-                <div class="form-group">
-                    <label for="imagefile">Upload your image: </label>
-                    <div class="input-group">
-                        <input type="file" name="imagefile" id="imagefile"/>
+        <div class="h-space2"></div>
+        <div class="row panel panel-default">
+            <div class="panel-heading">
+                Image upload
+            </div>
+            <div class="imageUploadSection panel-body">
+                <form class="uploadform " method="post" enctype="multipart/form-data"
+                      action='upload.php'>
+                    <div class="form-group">
+                        <label for="imagefile">Upload your image: </label>
+                        <div class="input-group">
+                            <input type="file" name="imagefile" id="imagefile"/>
+                        </div>
                     </div>
-                </div>
-                <input type="submit" value="Upload" class="btn btn-default" name="submitbtn" id="submitbtn">
-            </form>
-            <!-- The uploaded image will display here -->
-            <div id='viewimage'></div>
+                    <input type="submit" value="Upload" class="btn btn-default" name="submitbtn" id="submitbtn">
+                </form>
+                <!-- The uploaded image will display here -->
+                <div id='viewimage'></div>
+            </div>
         </div>
     </div>
 </div>
